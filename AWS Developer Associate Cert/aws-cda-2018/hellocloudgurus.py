624e46d89fab65756b7b32b6d295b6166e67cfef
Agrawal <a.a.agrawal@accenture.com>
2020-08-01 22:54:29 -0500

    updated on 8/1

---
+++ AWS Developer Associate Cert/aws-cda-2018/hellocloudgurus.py
@@ -0,0 +1,13 @@
+def lambda_handler(event, context):
+    print("In lambda handler")
+    
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Ankit Agrawal"
+    }
+    
+    return resp
+    
